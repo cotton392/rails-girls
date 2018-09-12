b60a16610bb5934fb7c0be8bf1ad6fde7bc535fa
kuraus <e1739@s.akashi.ac.jp>
2018-09-12 14:48:34 +0900

    hoge

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :comments
   get 'pages/info'
 	root to: redirect('/ideas')
-  resources :ideas
+  resources :ideas do
+  	resources :comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
